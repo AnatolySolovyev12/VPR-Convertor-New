dc90a1be5f7f6aebe31f95b00db833d0152e638e
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-01-23 20:32:58 +0300

    Добавьте файлы проекта.

---
+++ VPR Convertor New/main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+
+#include "table.h"
+
+int main(int argc, char* argv[]) {
+
+    QApplication app(argc, argv);
+
+    Table window;
+
+    window.resize(650, 450);
+    window.setWindowIcon(QIcon("icon.png"));
+    window.setWindowTitle("VPR Convertor by Solovev");
+    window.show();
+
+    return app.exec();
+}
